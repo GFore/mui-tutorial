c095959258332c18bf44e244da966555cce7e5ab
Greg Foreman <gfore@mindspring.com>
2020-10-10 18:23:41 -0400

    playing with MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          // disabled
+          variant="contained"
+          color="primary"
+          size="small"
+          onClick={() => {
+            let x = window.confirm('Yes or no?');
+            console.log('resp > ', x);
+          }}
         >
-          Learn React
-        </a>
+          Hello World
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          // size="small"
+          onClick={() => {
+            let x = window.confirm('Yes or no?');
+            console.log('resp > ', x);
+          }}
+        >
+          Hello World
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => {
+            let x = window.confirm('Yes or no?');
+            console.log('resp > ', x);
+          }}
+        >
+          Hello World
+        </Button>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
   );
